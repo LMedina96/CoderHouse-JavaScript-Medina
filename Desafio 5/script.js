f5e16eca99504fe4908b0b9f027e04c4aeec9b3f
Lucas Medina <lucasgmedina96@gmail.com>
2022-01-08 15:03:46 -0300

    Commit Clase 5

---
+++ Desafio 5/script.js
@@ -0,0 +1,35 @@
+let idMonto = document.getElementById("idMonto");
+let idIva = document.getElementById("idIva");
+let idTotal = document.getElementById("idTotal");
+let informe = [];
+let aux = 0;
+
+class Informe{
+    constructor(monto, iva, total){
+        this.monto = monto;
+        this.iva = iva;
+        this.total = total;
+    }
+}
+
+calcularMonto = () => {
+    idIva.value = "";
+    idTotal.value = "";
+    var monto = parseFloat(prompt("Ingresar monto del producto"));
+    idMonto.value = monto;
+
+}
+
+calcularIva = () => {
+    var totalIva = parseFloat(idMonto.value * 0.21);
+    idIva.value = totalIva
+}
+
+calcularTotal = () => {
+    idTotal.value = parseFloat(idMonto.value) + parseFloat(idIva.value);
+
+    informe[aux] = new Informe(idMonto.value, idIva.value, idTotal.value);
+    console.log(informe);
+
+    aux += 1;
+}
